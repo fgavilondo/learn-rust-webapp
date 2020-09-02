611258b9d4f9b7cfbe2658d3be69fe6f24f77c80
Fabio Gavilondo <fgavilondo@icloud.com>
2020-09-02 23:27:48 +1000

    Mutable application state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::sync::{Mutex, MutexGuard};
+
 use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u32 = 8088;
 
-// Application state, will be shared by multiple (requests processing) threads
+// Application state, will be shared by multiple (requests processing) threads.
+// Application state can be accessed with the web::Data<T> extractor where T is type of state.
+// Internally, web::Data uses Arc<T>, i.e. 'Atomically Reference Counted'.
+// Shared references in Rust disallow mutation by default, and Arc is no exception.
+// If you need to mutate through an Arc, use Mutex, RwLock, or one of the Atomic types.
 struct AppState {
-    teacher_name: String,
+    teacher_name: Mutex<String>
 }
 
 // A request handler is an async function that accepts zero or more parameters that can be extracted from a request
@@ -14,13 +20,11 @@ async fn handler_homepage() -> impl Responder {
     HttpResponse::Ok().body("This is the home page")
 }
 
-// Application state can be accessed (read-only) with the web::Data<T> extractor where T is type of state.
-// Internally, web::Data uses Arc<T>, i.e. 'Atomically Reference Counted'.
 async fn handler_teacher(data: web::Data<AppState>) -> impl Responder {
-    let teacher_name = &data.teacher_name;
-    // Shared references in Rust disallow mutation by default, and Arc is no exception.
-    // If you need to mutate through an Arc, use Mutex, RwLock, or one of the Atomic types.
-    // data.teacher_name = String::from("Another teacher");
+    let teacher_name: MutexGuard<String> = data.teacher_name.lock().unwrap(); // get MutexGuard
+    // to modify
+    // let mut teacher_name = data.teacher_name.lock().unwrap(); // get MutexGuard
+    // *teacher_name = String::from("Another teacher"); // access teacher_name inside MutexGuard
     HttpResponse::Ok().body(format!("The teacher is: {}", teacher_name))
 }
 
@@ -28,17 +32,21 @@ async fn handler_teacher(data: web::Data<AppState>) -> impl Responder {
 // your functions like so:
 #[get("/students")]
 async fn handler_students() -> impl Responder {
-    HttpResponse::Ok().body(format!("The students are: {}", "dipan david fabio"))
+    HttpResponse::Ok().body(format!("The students are: {}", "Claire David Louise"))
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Initialize application state, which is shared with all routes and resources within the same scope.
+    let app_state = web::Data::new(AppState {
+        teacher_name: Mutex::new(String::from("Mat")),
+    });
+
+    HttpServer::new(move || {
+        // move app_state into the closure
         App::new()
-            // Initialize application state, which is shared with all routes and resources within the same scope.
-            .data(AppState {
-                teacher_name: String::from("Mat"),
-            })
+            // register app_state
+            .app_data(app_state.clone())
             // register request handlers on a path with a method
             .route("/", web::get().to(handler_homepage))
             .route("/teacher", web::get().to(handler_teacher))
